c189735c7a1eb520e395fe889114caaba23b084c
Zurich Sabil <zurichsabil@gmail.com>
2026-03-05 21:20:24 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email !== "" && password !== "") {
+
+    window.location.href = "caritoko.html";
+  } else {
+    alert("Isi email dan password dulu ya!");
+  }
+});
